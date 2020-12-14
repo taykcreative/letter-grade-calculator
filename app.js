a00f147313c5ffda5fc864024c8516d8f3918b98
Tay <tayk.dev@gmail.com>
2020-12-13 21:57:02 -0800

    Add files via upload
    I tried adding JavaScript, but it's not ready yet. Declared button variables though. Need to add functions for algorithms.

---
+++ app.js
@@ -0,0 +1,18 @@
+// declare variables
+
+const btn1 = document.getElementsByClassName("one");
+const btn2 = document.getElementsByClassName("two");
+const btn3 = document.getElementsByClassName("three");
+const btn4 = document.getElementsByClassName("four");
+const btn5 = document.getElementsByClassName("five");
+const btn6 = document.getElementsByClassName("six");
+const btn7 = document.getElementsByClassName("seven");
+const btn8 = document.getElementsByClassName("eight");
+const btn9 = document.getElementsByClassName("nine");
+const btnClear = document.getElementsByClassName("clear");
+const btn0 = document.getElementsByClassName("zero");
+const btnDivide = document.getElementsByClassName("divide");
+
+// create an equation function
+    // when button is clicked, give a value to a variable
+
